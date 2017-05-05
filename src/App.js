dd091bfec5c9da4ea713842eaeeb8fd9f119fe97
Hope <www.420426962.com@qq.com>
2017-05-05 20:12:33 +0100

    add post via state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import _ from 'lodash';
-import { Link } from 'react-router';
+import 'bootstrap/dist/css/bootstrap.css';
 
+import './App.css';
 
 var Form = React.createClass({
-   getInitialState: function() {
-       return { title: '', link: ''};
-    },
+   // getInitialState: function() {
+   //     return { title: '', link: ''};
+   //  },
     handleTitleChange: function(e) {
        this.setState({title: e.target.value});
      },
@@ -15,103 +15,94 @@ var Form = React.createClass({
      },
      handleSubmit : function(e) {
         e.preventDefault();
-        var title = this.state.title.trim();
-        var link = this.state.link.trim();
+        var title=this.refs.title.value.trim();
+
+      var link=this.refs.link.value.trim();
         if (!title ) {
+
             return;
         }
         this.props.addHandler(title,link );
-        this.setState({title: '', link: ''});
+        
+this.refs.title.value='';
+      this.refs.link.value='';
+
+
      },
      render : function() {
        return (
-         <form style={{marginTop: '30px'}}>
+         <div className="container">
+                   <div className="row">
+                      <div className="col-md-6 col-md-offset-3"> <form style={{marginTop: '30px'}}>
             <h3>Add a new post</h3>
             <div className="form-group">
               <input type="text"
-                className="form-control" placeholder="Title"
-                value={this.state.title}
+                className="form-control" placeholder="Title" ref="title"
+               
                 onChange={this.handleTitleChange} ></input>
             </div>
             <div className="form-group">
               <input type="text"
-                 className="form-control" placeholder="Link"
-                 value={this.state.link}
+                 className="form-control" placeholder="Link" ref="link"
+                 
                  onChange={this.handleLinkChange} ></input>
             </div>
             <button type="submit" className="btn btn-primary"
                  onClick={this.handleSubmit} >Post</button>
           </form>
+           </div>
+         </div>
+         </div>
         );
       }
    });
 
-var NewsItem = React.createClass({
-    handleVote : function() {
-      this.props.upvoteHandler(this.props.post.id);
-    },
-    render : function() {
-        var lineStyle = {
-             fontSize: '20px', marginLeft: '10px'  };
-        var cursor = { cursor: 'pointer' } ;
-        var line ;
-        if (this.props.post.link ) {
-           line = <a href={this.props.post.link} >
-                        {this.props.post.title} </a> ;
-        } else {
-           line = <span>{this.props.post.title} </span> ;
-        }
-        return (
-          <div >
-            <span className="glyphicon glyphicon-thumbs-up" 
-                style={cursor} 
-                onClick={this.handleVote} ></span>
-            {this.props.post.upvotes}
-            <span style={lineStyle} >{line}<span>
-                <Link to={'/posts/' + this.props.post.id }>Comments</Link>
-              </span>
-            </span>
-          </div>  
-    );
-    }
-   }) ;
 
 var NewsList = React.createClass({
-    render : function() {
-      var items = this.props.posts.map(function(post,index) {
-         return <NewsItem key={index} post={post} 
-                    upvoteHandler={this.props.upvoteHandler}  /> ;
-        }.bind(this) )
-      return (
-        <div>
-              {items}
-              </div>
-        );
-    }
+   render:function(){
+  return(
+        <ul className="list-group"  >
+          {
+            this.props.posts.map(post =>{
+           return <li  post={post} key={post.id}><a href={post.link}> {post.title}</a> </li>
+          })
+        }
+        </ul>
+  
+  )
+ }
+
 }) ;  
 
 var HackerApp = React.createClass({ 
-    componentWillUnmount: function() {
-       api.persist() ;
+    getInitialState: function() {
+       return {
+       
+        posts:[
+        ]
+     }
+      
     },
-    addPost : function(t,l) {   
-      if (api.add(t,l)) {  
-         this.setState({});
-      }
+    
+      addPost:function(title,link){
+ 
+  console.log("add");
+  var newitem={
+    id:this.state.posts.length +1,
+    title:title,
+    link:link
+  }
+
+
+  this.setState({posts:this.state.posts.concat(newitem)});
     }, 
-    incrementUpvote : function(id) {
-      api.upvote(id) ;
-      this.setState({});
-    },    
+        
     render: function(){
-        var posts = _.sortBy(api.getAll(), function(post) {
-                return - post.upvotes;
-             }
-          );
+        
         return (
            <div >
-               <NewsList posts={posts} 
-                    upvoteHandler={this.incrementUpvote} />
+               <NewsList posts={this.state.posts}
+                    />
                <Form addHandler={this.addPost}  />
           </div>
         );
